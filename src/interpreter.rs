89350ad356d12d2ac421ac42973ec750297610e1
Den4200 <dennis@dennispham.me>
2021-08-18 12:37:34 -0400

    Implement `Interpreter::new` function.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,7 @@
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
+use crate::font::FONT_SET;
 use crate::instruction::{Address, Byte, IRegister, Register};
 
 pub struct Interpreter {
@@ -19,4 +20,32 @@ pub struct Interpreter {
     pub display: Canvas<Window>,
     pub vram: [u8; 2048],
     pub keys: [bool; 16],
+}
+
+impl Interpreter {
+    pub fn new(rom: Vec<u8>, display: Canvas<Window>) -> Interpreter {
+        let mut memory = [0; 4096];
+
+        for (i, byte) in rom.iter().enumerate() {
+            memory[i + 0x200] = *byte;
+        }
+
+        for (i, byte) in FONT_SET.iter().enumerate() {
+            memory[i] = *byte;
+        }
+
+        Interpreter {
+            regs: [0; 16],
+            i: 0,
+            delay_timer: 0,
+            sound_timer: 0,
+            memory,
+            stack: [0; 16],
+            pc: 0x200,
+            sp: 0,
+            display,
+            vram: [0; 2048],
+            keys: [false; 16],
+        }
+    }
 }
