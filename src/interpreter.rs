0703b542b00f5301b48ebd4f381d503d5168bad0
Den4200 <dennis@dennispham.me>
2021-08-18 16:06:16 -0400

    Add `pc_next` and `run_instruction` methods for `Interpreter`.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,7 +4,7 @@ use sdl2::render::Canvas;
 use sdl2::video::Window;
 
 use crate::font::FONT_SET;
-use crate::instruction::{Address, Byte, IRegister, Register};
+use crate::instruction::{Address, Byte, IRegister, Instruction, Register};
 
 pub struct Interpreter {
     pub regs: [Register; 16],
@@ -74,6 +74,183 @@ impl Interpreter {
 
         if self.sound_timer > 0 {
             self.sound_timer -= 1;
+        }
+    }
+
+    fn pc_next(&mut self) {
+        self.pc += 2;
+    }
+
+    pub fn run_instruction(&mut self, instruction: &Instruction) {
+        self.tick();
+        let mut should_increment = true;
+
+        match instruction {
+            Instruction::OP_00E0 => {
+                for index in 0..2048 {
+                    self.vram[index] = 0;
+                }
+            }
+            Instruction::OP_00EE => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp as usize];
+            }
+            Instruction::OP_1NNN(nnn) => {
+                self.pc = *nnn;
+                should_increment = false;
+            }
+            Instruction::OP_2NNN(nnn) => {
+                self.stack[self.sp as usize] = self.pc;
+                self.sp += 1;
+                self.pc = *nnn;
+                should_increment = false;
+            }
+            Instruction::OP_3XNN(vx, nn) => {
+                if self.regs[*vx as usize] == *nn {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_4XNN(vx, nn) => {
+                if self.regs[*vx as usize] != *nn {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_5XY0(vx, vy) => {
+                if self.regs[*vx as usize] == self.regs[*vy as usize] {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_6XNN(vx, nn) => {
+                self.regs[*vx as usize] = *nn;
+            }
+            Instruction::OP_7XNN(vx, nn) => {
+                let index = *vx as usize;
+                self.regs[index] = self.regs[index].wrapping_add(*nn);
+            }
+            Instruction::OP_8XY0(vx, vy) => {
+                self.regs[*vx as usize] = self.regs[*vy as usize];
+            }
+            Instruction::OP_8XY1(vx, vy) => {
+                self.regs[*vx as usize] |= self.regs[*vy as usize];
+            }
+            Instruction::OP_8XY2(vx, vy) => {
+                self.regs[*vx as usize] &= self.regs[*vy as usize];
+            }
+            Instruction::OP_8XY3(vx, vy) => {
+                self.regs[*vx as usize] ^= self.regs[*vy as usize];
+            }
+            Instruction::OP_8XY4(vx, vy) => {
+                let x = self.regs[*vx as usize];
+                let y = self.regs[*vy as usize];
+
+                self.regs[*vx as usize] = x.wrapping_add(y);
+                self.regs[0xF] = (x as u16 + y as u16 > u8::MAX as u16) as u8;
+            }
+            Instruction::OP_8XY5(vx, vy) => {
+                let x = self.regs[*vx as usize];
+                let y = self.regs[*vy as usize];
+
+                self.regs[*vx as usize] = x.wrapping_sub(y);
+                self.regs[0xF] = (x > y) as u8;
+            }
+            Instruction::OP_8XY6(vx, vy) => {
+                self.regs[*vx as usize] = self.regs[*vy as usize] >> 1;
+                self.regs[0xF] = self.regs[*vy as usize] & 1;
+            }
+            Instruction::OP_8XY7(vx, vy) => {
+                let x = self.regs[*vx as usize];
+                let y = self.regs[*vy as usize];
+
+                self.regs[*vx as usize] = y.wrapping_sub(x);
+                self.regs[0xF] = (y > x) as u8;
+            }
+            Instruction::OP_8XYE(vx, vy) => {
+                self.regs[*vx as usize] = self.regs[*vy as usize] << 1;
+                self.regs[0xF] = self.regs[*vy as usize] >> 7;
+            }
+            Instruction::OP_9XY0(vx, vy) => {
+                if self.regs[*vx as usize] != self.regs[*vy as usize] {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_ANNN(nnn) => {
+                self.i = *nnn;
+            }
+            Instruction::OP_BNNN(nnn) => {
+                self.pc = *nnn + self.regs[0] as u16;
+                should_increment = false;
+            }
+            Instruction::OP_CXNN(vx, nn) => {
+                self.regs[*vx as usize] = rand::random::<u8>() & *nn;
+            }
+            Instruction::OP_DXYN(vx, vy, n) => {
+                self.regs[0xF] = 0;
+
+                for byte in 0..*n {
+                    let y = (self.regs[*vy as usize] + byte) % 32;
+
+                    for bit in 0..8 {
+                        let x = (self.regs[*vx as usize] + bit) % 64;
+                        let color = (self.memory[(self.i + byte as u16) as usize] >> (7 - bit)) & 1;
+
+                        self.regs[0xF] |= color & self.vram[(y * 32 + x) as usize];
+                        self.vram[(y * 32 + x) as usize] ^= color;
+                    }
+                }
+            }
+            Instruction::OP_EX9E(vx) => {
+                if self.keys[self.regs[*vx as usize] as usize] == 1 {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_EXA1(vx) => {
+                if self.keys[self.regs[*vx as usize] as usize] != 1 {
+                    self.pc_next();
+                }
+            }
+            Instruction::OP_FX07(vx) => {
+                self.regs[*vx as usize] = self.delay_timer;
+            }
+            Instruction::OP_FX0A(vx) => {
+                if self.keys[self.regs[*vx as usize] as usize] != 1 {
+                    should_increment = false;
+                }
+            }
+            Instruction::OP_FX15(vx) => {
+                self.delay_timer = self.regs[*vx as usize];
+            }
+            Instruction::OP_FX18(vx) => {
+                self.sound_timer = self.regs[*vx as usize];
+            }
+            Instruction::OP_FX1E(vx) => {
+                self.i += self.regs[*vx as usize] as u16;
+            }
+            Instruction::OP_FX29(vx) => {
+                self.i = (self.regs[*vx as usize] * 5) as u16;
+            }
+            Instruction::OP_FX33(vx) => {
+                let x = self.regs[*vx as usize];
+
+                self.memory[self.i as usize] = x / 100;
+                self.memory[self.i as usize + 1] = x / 10 % 10;
+                self.memory[self.i as usize + 2] = x % 10;
+            }
+            Instruction::OP_FX55(vx) => {
+                for index in 0..=*vx as usize {
+                    self.memory[self.i as usize + index] = self.regs[index];
+                }
+                self.i += *vx as u16 + 1;
+            }
+            Instruction::OP_FX65(vx) => {
+                for index in 0..=*vx as usize {
+                    self.regs[index] = self.memory[self.i as usize + index];
+                }
+                self.i += *vx as u16 + 1;
+            }
+        };
+
+        if should_increment {
+            self.pc_next();
         }
     }
 }
