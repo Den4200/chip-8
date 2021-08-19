c8bb33935c9694cd35732d098ba1b857dfafea6a
Den4200 <dennis@dennispham.me>
2021-08-18 23:35:24 -0400

    Create `load_rom` function.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::fs::File;
+use std::io::{Error, Read};
 mod font;
 mod instruction;
 mod interpreter;
 mod masks;
 
+fn load_rom(filename: &String) -> Result<Vec<u8>, Error> {
+    let mut file = File::open(filename)?;
+    let mut rom = Vec::new();
+
+    file.read_to_end(&mut rom)?;
+    Ok(rom)
+}
 fn main() {
     println!("Hello, world!");
 }
